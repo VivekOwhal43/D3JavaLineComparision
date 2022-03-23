3a1b53395182092969e74512a3cae82d656107c8
VivekOwhal43 <vivekowhal.co@gmail.com>
2022-03-23 17:04:25 +0530

    [Vivek] Line Length Class Pushed

---
+++ LineMethods/LineData.cs
@@ -0,0 +1,36 @@
+using System;
+namespace LineConverter;
+public class LineData
+{
+    public int x1, y1, x2, y2;
+    public double length_of_line;
+    public void getAxisPoints()     //Method fo Accepting values from user.
+    {
+        Console.WriteLine("Enter value for X1: ");
+        x1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter value for Y1: ");
+        y1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter value for X2: ");
+        x2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter value for Y2: ");
+        y2 = Convert.ToInt32(Console.ReadLine());
+    }
+
+    public void showAxisPoints()        // method for showing entered values
+    {
+        Console.WriteLine("============Entered Values Are =================");
+        Console.WriteLine("X1 = "+x1);
+        Console.WriteLine("Y1 = "+y1);
+        Console.WriteLine("X2 = "+x2);
+        Console.WriteLine("Y2 = "+y2);
+        length_of_line = Math.Sqrt((x2-x1)+(y2-y1));
+        length_of_line= Math.Round(length_of_line);
+        if(length_of_line<0){
+            Console.WriteLine("Enter correct values");
+        }
+        //length_of_line = Math.abs(length_of_line);
+        else{
+        Console.WriteLine("Length of Line: "+length_of_line+" Cm");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection.Metadata;
+using LineConverter;
+//using LineConverter.LineMethods;
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LineData lineData = new LineData();
+            //lineData.number=5;
+            lineData.getAxisPoints();
+            lineData.showAxisPoints();
+        }
+    }
+}
