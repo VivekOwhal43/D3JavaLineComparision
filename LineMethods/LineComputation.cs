ede09244b5c48ddecafb3d0548e63a7b1302c38c
VivekOwhal43 <vivekowhal.co@gmail.com>
2022-03-28 15:19:54 +0530

    [Vivek] Completed UC4 (Using OOP Concepts)

--- LineMethods/LineComputation.cs
+++ LineMethods/LineComputation.cs
@@ -1,22 +1,22 @@
 using System;
-namespace LineConverter;
-public class LineData
+namespace LineConverter.LineMethods;
+
+public class LineComputation : LineData
 {
-    public int x1, y1, x2, y2;
-    public double length_of_line;
-    public void getAxisPoints()     //Method fo Accepting values from user.
+    new public void getAxisPoints()
     {
+        Console.WriteLine("============ Entere Points =================");
         Console.WriteLine("Enter value for X1: ");
         x1 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter value for Y1: ");
-        y1 = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Enter value for X2: ");
         x2 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter value for Y2: ");
+        Console.WriteLine("Enter value for Y1: ");
+        y1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter value for Y1: ");
         y2 = Convert.ToInt32(Console.ReadLine());
     }
 
-    public void showAxisPoints()        // method for showing entered values
+    new public void showAxisPoints()
     {
         Console.WriteLine("============Entered Values Are =================");
         Console.WriteLine("X1 = " + x1);
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using System.Reflection.Metadata;
 using System.Security.Cryptography;
 using LineConverter;
+using LineConverter.LineMethods;
 //using LineConverter.LineMethods;
 namespace ConsoleApplication
 {
@@ -12,18 +13,18 @@ namespace ConsoleApplication
         LineData lineData = new LineData();
         LineData lineData2 = new LineData();
         LineData lineData3 = new LineData();
-        public void uc1()
+        public void acceptPointFromUser()
         {
             Console.WriteLine("Use Case 1");
-            
+
             lineData.getAxisPoints();
             lineData.showAxisPoints();
         }
 
-        public void uc2()
+        public void checkLengthIsEqual()
         {
             Console.WriteLine("Enter information for 2 lines: ");
-            
+
             Console.WriteLine("Data For First Line:");
             lineData2.getAxisPoints();
             Console.WriteLine("Data For Second Line:");
@@ -43,7 +44,7 @@ namespace ConsoleApplication
                 Console.WriteLine("Line is Not Equal");
             }
         }
-        public void uc3()
+        public void checkIfLessGreaterOREqual()
         {
             Console.WriteLine("Data For First Line:");
             lineData2.getAxisPoints();
@@ -52,36 +53,65 @@ namespace ConsoleApplication
             double line1 = lineData2.length_of_line;
             double line2 = lineData3.length_of_line;
             var result = line1.CompareTo(line2);
-            if(result>0)
-            Console.WriteLine("First line is greater than Second line");
-
+            if (result > 0)
+            {
+                Console.WriteLine("First line is greater than Second line");
+            }
             else if (result < 0)
+            {
                 Console.WriteLine("First line is less than Second line");
-
+            }
             else
                 Console.WriteLine("Both Line are Equal");
-            
+
         }
         static void Main(string[] args)
         {
             Program p1 = new Program();
             Console.WriteLine("Enter Your Choice: \n1. Obtain Lenght of Line \n2.Chceck if two lines are equal \n3.Comparison of lines (Equal, Less or Greater)");
+            Console.WriteLine("4.Show Result Using OOP Concepts");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
-                
+
                 case 1:
-                p1.uc1();
-                    break;
+                    p1.acceptPointFromUser();
+                break;
 
                 case 2:
-                p1.uc2();
-                
-                    break;
+                    p1.checkLengthIsEqual();
+
+                break;
 
                 case 3:
-                p1.uc3();
-                    break;
+                    p1.checkIfLessGreaterOREqual();
+                break;
+
+                case 4:
+                    // Data for line 1
+                    LineComputation lineComputation = new LineComputation();
+                    lineComputation.getAxisPoints();
+                    lineComputation.showAxisPoints();
+
+                    // Dta for line 2
+                    LineComputation lineComputation1 = new LineComputation();
+                    lineComputation1.getAxisPoints();
+                    lineComputation1.showAxisPoints();
+                    double line1 = lineComputation.length_of_line;
+                    double line2 = lineComputation1.length_of_line;
+                    var result = line1.CompareTo(line2);
+                    if (result > 0)
+                    {
+                        Console.WriteLine("First line is greater than Second line");
+                    }
+                    else if (result < 0)
+                    {
+                        Console.WriteLine("First line is less than Second line");
+                    }
+                    else
+                        Console.WriteLine("Both Line are Equal");
+
+                break;
             }
         }
     }
